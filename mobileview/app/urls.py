f1e3bb219e3afe04dd9a4c0462791f16c2c2722c
DesignWithShamil <shamilpazhamkulam@gmail.com>
2025-08-21 11:33:01 +0530

    size and view all edit

--- mobileview/app/urls.py
+++ mobileview/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home_nav/', views.home_nav, name='home_nav'),
+    path('home/', views.home, name='home'),
+    path('', views.home_nav, name='home_nav'),
 ]
